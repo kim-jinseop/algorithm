db2cef425657266b03899d3a9392e3c4a6947d17
Kimjs-Home <uskhbs@naver.com>
2022-03-17 01:52:01 +0900

    Q Complate

--- Greedy/1.py
+++ Greedy/1.py
@@ -1,4 +1,23 @@
-a=10
-b=20 
+n, m, k = map(int,input().split())
+data = list(map(int,input().split()))
 
-print(a+b)
+data.sort(reverse=True)
+
+max = data[0]
+max_2 = data[1]
+
+i = 1
+result = 0
+
+while i<=m :
+
+    if i%(k+1)==0 :
+        result = result + max_2
+    else :
+        result = result + max 
+    
+    i += 1
+    print(result)
+
+
+print("답:" + str(result))
