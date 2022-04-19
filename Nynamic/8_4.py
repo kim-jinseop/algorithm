aa09e640b217c513d09f3d990a0d180e65abb146
uskhbs <uskhbs@naver.com>
2022-04-19 15:25:12 +0900

    [Complete] 8_4.py

--- Nynamic/8_4.py
+++ Nynamic/8_4.py
@@ -7,3 +7,15 @@ for i in range(n) :
     array.append(int(input()))
     
     
+d = [10001] * 10001
+d[0] = 0
+
+for i in range(n) : 
+    for j in range(array[i],m+1) :
+        d[j] = min(d[j],d[j-array[i]]+1)
+        
+        
+if d[m]==10001 :
+    print(-1)
+else :
+    print(d[m])        
