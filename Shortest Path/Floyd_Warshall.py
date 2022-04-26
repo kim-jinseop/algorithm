54f4af8d516d1ec560962114053a0b8363578d97
Kimjs-Home <uskhbs@naver.com>
2022-04-27 01:10:58 +0900

    [Complete] Floyd_Warshall.py

--- Shortest Path/Floyd_Warshall.py
+++ Shortest Path/Floyd_Warshall.py
@@ -10,14 +10,22 @@ array = [[INF] * (n+1) for _ in range(n+1)] #리스트 컴프리핸션
 for i in range(1,n+1) :
     array[i][i] = 0
 
-#간선에 대한 정보 입력
-for i in range(m) :
+# 간선에 대한 정보 입력
+for I in range(m) :
     a,b,c = map(int,input().split())
     array[a][b] = c
         
+for k in range(1,n+1) :
+    for a in range(1,n+1) :
+        for b in range(1,n+1) :
+            array[a][b] = min(array[a][b], array[a][k] + array[k][b])
+
 
 # 출력
 for i in range(1,n+1) :
     for j in range(1,n+1) :
-        print(array[i][j],end=' ')
+        if array[i][j] == INF :
+            print('x',end=' ')
+        else :
+            print(array[i][j],end=' ')
     print()    
