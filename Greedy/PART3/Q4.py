ab09074488505a28b089cc221f51aab500272b81
uskhbs <uskhbs@naver.com>
2022-05-10 13:08:31 +0900

    [Complete] Q4.py

--- Greedy/PART3/Q4.py
+++ Greedy/PART3/Q4.py
@@ -1,11 +1,13 @@
 # 만들 수 없는 금액
 n = int(input())
 array = list(map(int,input().split()))
-
 array.sort()
 
 target = 1
-
-while True :
+for i in array :
+    if target < i :
+        break
+    else :
+        target += i
     
-    
+print(target)    
