0a9f46c67dbba0ce55b48aa0e32728def7190b1f
uskhbs <uskhbs@naver.com>
2022-05-13 17:32:01 +0900

    [Complete] Q9.py

---
+++ Implementation/PART3/Q9.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60057 
+# 위 링크에서 문제풀기
+
+def solution(s):
+    answer = 0
+    
+    length = len(s)
+    
+    
+    for i in range(length) :
+        
+        for j in s(length)
+        
+        
+        
+    return answer
