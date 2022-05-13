cd6109ae218df2445e6f993a7725ad11be32a06d
uskhbs <uskhbs@naver.com>
2022-05-13 18:43:32 +0900

    [Fix] Q9.py

--- Implementation/PART3/Q9.py
+++ Implementation/PART3/Q9.py
@@ -1,16 +1,15 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60057 
 # 위 링크에서 문제풀기
 
-def solution(s):
-    answer = 0
-    
+def solution(s):    
     length = len(s)
-    
-    
-    for i in range(length) :
         
-        for j in s(length)
+    for step in range(1,length//2+1) :
+        L_group = s[0:step]
+        for i in range(1,length,step) :
+            R_group = s[step:]
+            
+            if L_group == R_group :
+            
         
-        
-        
-    return answer
+    return 
