b8696e2d8ccd95ecfe4caef5145f9cda3caaacb9
Kimjs-Home <uskhbs@naver.com>
2022-05-17 22:01:51 +0900

    [Fix] Q10.py

--- Implementation/PART3/Q10.py
+++ Implementation/PART3/Q10.py
@@ -1,1 +1,19 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60059
+
+def solution(key, lock):
+    answer = True
+    return answer
+
+
+def Rotation(array) :
+    length = len(array)
+
+    result = [[0] * length for _ in range(length)] 
+    for i in range(length) :
+        for j in range(length) :
+            result[j][length - 1- i] = array[i][j]
+    return result 
+
+
+x = [[0,1,0],[2,1,0],[3,1,2]]
+print(Rotation(x))
