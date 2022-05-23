0b31f1860f7f81c52ec1dccebe5af3e615022e31
Kimjs-Home <uskhbs@naver.com>
2022-05-24 00:52:47 +0900

    [Fix] Q12.py

--- Implementation/PART3/Q12.py
+++ Implementation/PART3/Q12.py
@@ -1,2 +1,28 @@
 # 기둥과 보 설치
 # https://programmers.co.kr/learn/courses/30/lessons/60061
+
+
+
+def solution(n, build_frame):
+
+    data = [[0]*n for _ in range(n)]
+
+    result = []
+    for frame in build_frame :
+        x,y = frame[0] , frame[1]
+        a = frame[2] # 0: 기둥 or 1: 보
+        b = frame[3] # 0: 삭제 or 1: 설치
+
+        if a==0 and b == 1:
+            result.append([x,y,a])
+        elif a==1 and b ==1:
+            if 
+
+
+    return 
+
+def column(data):
+
+    return
+
+def 
