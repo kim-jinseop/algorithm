2fb3e750c76c2fcc4dec0d964077fa025f50cc94
Kimjs-Home <uskhbs@naver.com>
2022-05-25 22:31:33 +0900

    [Complete] Q12.py

--- Implementation/PART3/Q12.py
+++ Implementation/PART3/Q12.py
@@ -2,17 +2,11 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60061
 
 result = []
-n = 5
-build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
-
 def solution(n, build_frame):
 
     # map data 기둥, 보 유무 
     # 1은 기둥이 설치된 곳으로 기둥과 보를 설치할 수 있는 위치
     # 2는 보가 설치된 곳으로 기둥은 설치 할 수 있으며, 보는 y+2 가 '2' 일때 설치 가능
-    data = [[0]*(n+1) for _ in range(n+1)]
-    for i in range(n+1) :
-        data[0][i] = 1 # 바닥은 1 로 초기화 
 
     for frame in build_frame :
         x,y = frame[0] , frame[1] # 가로, 세로
@@ -20,38 +14,32 @@ def solution(n, build_frame):
         b = frame[3] # 0: 삭제 or 1: 설치
 
         if b==1 :
-            create(data,x,y,a)
+            result.append([x,y,a])
+            if cheak(result) == False :
+                result.remove([x,y,a])
+
         elif b==0 :
-            remove(data,x,y,a)
+            result.remove([x,y,a])
+            if cheak(result) == False :
+                result.append([x,y,a])
 
-    return result
+    return sorted(result)
 
-def create(data,x,y,a):
-    if a==0:
-        if data[y][x] == 1 or data[y][x] == 2 : 
-            data[y+1][x] = 1
-            result.append([x,y,a])
-            
-    elif a==1 :
-        if data[y][x] == 1 :
-            data[y][x+1] = 2
-            result.append([x,y,a])
-            
-        elif data[y][x+1] == 1 : 
-            data[y][x] = 2
-            result.append([x,y,a])
-            
-        elif data[y][x]==2 and data[y][x+1]==2 :
-            result.append([x,y,a])    
-            
-    return
+def cheak(result):
+    
+    for x,y,item in result :
+        if item == 0 : # 기둥
+            # 바닥일 때, 보의 한쪽 끝일 때, 다른 기둥 위일 때
+            if y==0 or [x-1,y,1] in result or [x,y,1] in result or [x,y-1,0] in result :
+                continue
+            else :
+                return False
 
-def remove(data,x,y,a):
-    # if a==0 :
-    #     if data[y+1][x] != 1 and (data[y+1][x-1] == 2 and data[y+1][x-1] == 2) :
-    #         result.remove([x,y,a])          
-    # elif a==1 :
-        
-    # return
+        elif item == 1 : # 보
+            # 기둥 위 일때, 오른쪽에 기둥이 있을때, 양쪽에 보가 있을 때 
+            if [x,y-1,0] in result or [x+1,y-1,0] in result or ([x-1,y,1] in result and [x+1,y,1] in result):
+                continue
+            else :
+                return False
 
-print(solution(n, build_frame))
+    return True
