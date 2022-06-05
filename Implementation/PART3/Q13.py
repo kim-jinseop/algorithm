18d4eec2c9fa7041a3b4d001fc74e2c2bbffccd6
Kimjs-Home <uskhbs@naver.com>
2022-06-05 19:39:27 +0900

    [Complete] Q13.py

--- Implementation/PART3/Q13.py
+++ Implementation/PART3/Q13.py
@@ -7,13 +7,28 @@ n,m = map(int,input().split())
 house = []
 chicken = []
 
+def serch_sum(candidate) :
+    result = 0
+    for hx, hy in house :
+        temp = 1e9
+        for cx, cy in candidate:
+            temp = min(temp, abs(hx-cx)+abs(hy-cy))
+
+        result += temp
+    return result
+
 for r in range(n) :
     array = list(map(int,input().split()))
     for c in range(n) :
         if array[c] == 1 :
             house.append((r,c))
         elif array[c] == 2 :
-            chicken.append((r.c))
+            chicken.append((r,c))
 
-combi = list(combinations(chicken, m))
+candidates = list(combinations(chicken, m))
 
+result = 1e9
+for candidate in candidates :
+    result = min(result, serch_sum(candidate))
+
+print(result)
