97ae97f26c4da91fe8c16f7384a990e5ec345bef
Kimjs-Home <uskhbs@naver.com>
2022-06-06 19:39:27 +0900

    [Fix] Q13.py add commnet

--- Implementation/PART3/Q13.py
+++ Implementation/PART3/Q13.py
@@ -10,9 +10,9 @@ chicken = []
 def serch_sum(candidate) :
     result = 0
     for hx, hy in house :
-        temp = 1e9
+        temp = 1e9 # 1e9 : 무한과 유사한 값
         for cx, cy in candidate:
-            temp = min(temp, abs(hx-cx)+abs(hy-cy))
+            temp = min(temp, abs(hx-cx)+abs(hy-cy)) # abs : 절대값
 
         result += temp
     return result
@@ -25,7 +25,7 @@ for r in range(n) :
         elif array[c] == 2 :
             chicken.append((r,c))
 
-candidates = list(combinations(chicken, m))
+candidates = list(combinations(chicken, m)) # combiations : 경우의 수를 뽑아낸다(조합)
 
 result = 1e9
 for candidate in candidates :
